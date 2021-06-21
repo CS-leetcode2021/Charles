f59deea699c7dc5fedeac515efd43a87a59c3393
Gnomeshgh <tanghe994@gmail.com>
2021-06-21 21:23:34 +0800

    2021.06.21: byte_dance\3\93

--- byte_dance/lengthOfLongstSubstring.go
+++ byte_dance/lengthOfLongstSubstring.go
@@ -1,7 +1,69 @@
 package main
+
+import (
+	"strings"
+)
+
 /**
  *  @ClassName:lengthOfLongstSubstring
- *  @Description:TODO
+ *  @Description:字节 3 无重复字符的最长子串 滑动窗口解决
  *  @Author:jackey
  *  @Create:2021/6/21 下午7:37
  */
+
+func length(s string) int {
+	// 记录每个字符是否出现过
+	memo := map[byte]int{}
+	n := len(s)
+	// 右指针初始化为-1的下标索引
+	// ans代表结果
+	rp, res := 0, 0
+	// i就是左指针边界
+	for lp := 0; lp < n; lp++ {
+		// 左指针移动需要删除之前的第一个字符，因为已经出现相同字符了
+		if lp != 0 {
+			delete(memo, s[lp-1])
+		}
+		// 右指针移动，并且map中没有出现，则代表是没有重复字符出现的
+		for rp < n && memo[s[rp]] == 0 {
+			memo[s[rp]]++
+			rp++
+		}
+		// 保存结果的最大值
+		res = maxString(res, rp-lp)
+	}
+	return res
+}
+
+func maxString(x, y int) int {
+	if x < y {
+		return y
+	}
+
+	return x
+}
+
+// 双100% 窗口并不减小，只增不减
+// 两个指针表示窗口大小，遍历以此字符串，窗口在遍历过程中滑动或增大
+// 1、窗口内没有重复字符：此时判断i+1和end的关系，超过表示遍历到窗口之外了，增大窗口的大小
+// 2、窗口内出现重复字符：左右两个指针都增大，滑动窗口位置到重复字符的后一位
+// 3、遍历结束，返回end-start的大小即可
+func lenght2(s string) int {
+	n := len(s)
+	lp , rp := 0,0
+	for i := 0; i < n; i++ {
+		index := strings.Index(s[lp:i],string(s[i]))
+
+		if index == -1 {
+			if i+1 > rp {
+				rp = i+1
+			}
+		}else {
+			lp += index+1
+			rp += index+1
+		}
+	}
+
+	return rp-lp
+}
+// 如果需要返回具体的字符串，只需要在窗口增大的时候记录start的指针即可
--- byte_dance/restoreIpAddresses.go
+++ byte_dance/restoreIpAddresses.go
@@ -1,7 +1,65 @@
 package main
+
+import "strconv"
+
 /**
  *  @ClassName:restoreIpAddresses
- *  @Description:TODO
+ *  @Description:字节 93 复原IP地址；解决方法：回溯；回溯会穷举所有的节点，通常用于解决找出所有的可能的组合
+ *  【https://www.cis.upenn.edu/~matuszek/cit594-2012/Pages/backtracking.html】
  *  @Author:jackey
  *  @Create:2021/6/21 下午8:51
  */
+
+const SegCount = 4
+
+var (
+	ans []string
+	segments []int
+)
+
+func restoreIP(s string) []string {
+	segments = make([]int,SegCount)
+	ans = []string{}
+	dfs(s,0,0)
+	return ans
+}
+
+func dfs(s string, segID, segStart int)  {
+	// 如果找到了4段IP地址，并且遍历完了所有的字符串，那么就是一种答案，将答案进行拼接即可
+
+	if segID == SegCount {	// 等于4段ID
+		if segStart == len(s) {
+			ipAddr := ""
+			for i := 0; i < SegCount; i++ {	// 进行IP拼接
+				ipAddr += strconv.Itoa(segments[i])
+				if i != SegCount-1 {
+					ipAddr += "."
+				}
+			}
+			ans = append(ans,ipAddr)	// 所有的结果
+		}
+		return
+	}
+
+	// 还未寻到第四段，便已经是遍历结束
+	if segStart == len(s) {
+		return
+	}
+
+	if s[segStart] == '0' {	// 如果是0,那么该段便是0
+		segments[segID] = 0
+		dfs(s,segID+1,segStart+1)
+	}
+	// 一般情况，枚举每一种可能性并递归
+	addr := 0
+	for segEnd := segStart; segEnd < len(s); segEnd++ {
+		addr = addr * 10 + int(s[segEnd] - '0')
+		if addr > 0 && addr <= 0xFF {
+			segments[segID] = addr
+			dfs(s, segID + 1, segEnd + 1)
+		} else {
+			break
+		}
+	}
+
+}
